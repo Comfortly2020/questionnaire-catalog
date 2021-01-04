7fbdd5887f6359e3fbb8e6ffd12fe4beb72486f7
davidvalic <david.valic@infinum.co>
2021-01-04 19:10:52 +0100

    Add consul to dependencies and update AnswerDataConverter

--- models/src/main/resources/sql-scripts/init_questionnaire.sql
+++ models/src/main/resources/sql-scripts/init_questionnaire.sql
@@ -1,1 +1,1 @@
-INSERT INTO questionnaire_data (question, type, possible_answers, active) VALUES ('How old are you?', 'NUMBER', null, true);
+INSERT INTO questionnaire_data (question, type, possible_answers, active) VALUES ('On a scale from 1 to 6, how are you feeling?', 'NUMBER', null, true);
